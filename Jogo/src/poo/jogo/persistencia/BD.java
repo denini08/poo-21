d0546d49fbc8e8399ac4ec4413883a386f80c5a3
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-03-18 19:51:09 -0300

    BD - incompleto

--- Jogo/src/poo/jogo/persistencia/BD.java
+++ Jogo/src/poo/jogo/persistencia/BD.java
@@ -1,5 +1,41 @@
 package poo.jogo.persistencia;
 
+import org.bson.Document;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class BD {
 
+	public static void main(String[] args) {
+		
+		
+		MongoClient mongoClient = new MongoClient();
+		System.out.println("Conexão com o MONGO feita!");
+		MongoDatabase DB = mongoClient.getDatabase("jogo21");
+		System.out.println("conexão com o BD feita!" + DB.getName());
+		
+		MongoCollection<Document> col = DB.getCollection("jogo");
+		
+		Document documento = new Document("nome","thomás").append("carteira", 1);
+		//col.insertOne(documento);
+
+		System.out.println("LER");
+		
+		FindIterable<Document> res = col.find();
+		
+		 for (Document document : res) {
+	            System.out.println(document.toJson());
+	        }
+
+   
+   
+	}
 }
+
+
